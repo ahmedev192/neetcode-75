dc52f7c08163288cf8add56eb90567339049f609
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-02 22:18:25 +0300

    Valid Palindrome

--- Two_Pointers/Valid_Palindrome.cs
+++ Two_Pointers/Valid_Palindrome.cs
@@ -1,1 +1,20 @@
 // Solution for Valid Palindrome
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        var cleaned = new string(
+            s.ToLower().Where(char.IsLetterOrDigit).ToArray()
+        );
+
+        int left = 0, right = cleaned.Length - 1;
+
+        while (left < right)
+        {
+            if (cleaned[left++] != cleaned[right--])
+                return false;
+        }
+
+        return true;
+    }
+}
