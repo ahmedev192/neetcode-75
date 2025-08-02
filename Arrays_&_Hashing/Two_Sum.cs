ccbfc8143b4f8952f13ba5dc99a252fe6d63469b
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-02 06:55:26 +0300

    Two_Sum

--- Arrays_&_Hashing/Two_Sum.cs
+++ Arrays_&_Hashing/Two_Sum.cs
@@ -1,1 +1,25 @@
 // Solution for Two Sum
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            if (!map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        throw new ArgumentException("No solution found");
+    }
+}
