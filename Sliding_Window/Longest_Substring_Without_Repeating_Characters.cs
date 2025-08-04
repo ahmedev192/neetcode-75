743eb884eb1f5b9d3d26defceaeaaf77fb01a029
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 19:04:14 +0300

    Longest Substring Without Repeating Characters

--- Sliding_Window/Longest_Substring_Without_Repeating_Characters.cs
+++ Sliding_Window/Longest_Substring_Without_Repeating_Characters.cs
@@ -1,1 +1,23 @@
 // Solution for Longest Substring Without Repeating Characters
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var unique = new HashSet<char>();
+        int left = 0, right = 0, maxLength = 0;
+        while (right < s.Length)
+        {
+            if (!unique.Contains(s[right]))
+            {
+                unique.Add(s[right++]);
+                maxLength = Math.Max(maxLength, unique.Count);
+            }
+            else
+            {
+                unique.Remove(s[left++]);
+            }
+        }
+        return maxLength;
+
+    }
+}
