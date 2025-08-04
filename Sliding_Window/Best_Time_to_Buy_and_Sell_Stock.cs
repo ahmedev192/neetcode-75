201c8fd855f324a9bf2b7f9c2642b3d4bb35eb20
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 14:50:25 +0300

    Best Time to Buy and Sell Stock

--- Sliding_Window/Best_Time_to_Buy_and_Sell_Stock.cs
+++ Sliding_Window/Best_Time_to_Buy_and_Sell_Stock.cs
@@ -1,1 +1,19 @@
 // Solution for Best Time to Buy and Sell Stock
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (var price in prices)
+        {
+            if (price < minPrice)
+                minPrice = price;
+            else
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
