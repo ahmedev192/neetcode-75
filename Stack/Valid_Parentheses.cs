0c62ca83503772fe39d603bff240948ea743d80f
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-05 15:19:11 +0300

    Valid Parentheses

--- Stack/Valid_Parentheses.cs
+++ Stack/Valid_Parentheses.cs
@@ -1,1 +1,32 @@
-// Solution for Valid Parentheses
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return true;
+
+        var stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char top = stack.Pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
