7e0421b0d03595ee384a530708d7303eb39c0f12
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-08 06:44:42 +0300

    Reverse Linked List

--- Linked_List/Reverse_Linked_List.cs
+++ Linked_List/Reverse_Linked_List.cs
@@ -1,1 +1,24 @@
 // Solution for Reverse Linked List
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+
+        ListNode prev = null;
+        var current = head;
+        while (current != null)
+        {
+            var nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+
+        }
+
+
+        return prev;
+
+
+    }
+}
